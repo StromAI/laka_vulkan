a5c67c62777bdc3d4627b7607dfd7f6440b30e05
jack break <1318405440@qq.com>
2018-09-27 17:44:55 +0800

    将数据入库,还差前提条件未导入.

---
+++ laka_vulkan/valid_usage_json2sqlite3.py
@@ -0,0 +1,24 @@
+﻿import json
+import sqlite3
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+j = json.load(open("validusage.json"))
+
+vk_db = sqlite3.connect("vk_1.1.85.db")
+c = vk_db.cursor()
+
+count =0
+for api_struct_name in j:
+    for tj in j[api_struct_name]:
+        for parmater_list in j[api_struct_name][tj]:
+            count = count + 1
+            c.execute(
+                "INSERT INTO sub (sub_name,as_name,valid_usage) VALUES (?, ?, ?)",
+                (parmater_list["vuid"],api_struct_name,parmater_list["text"]))
+
+print count
+vk_db.commit()
